a7d93b95eb4ba99a2c269f0f5a0c1441f9a6dc72
youliangdao <youliangdaojing5@gmail.com>
2022-08-06 20:36:18 +0900

    Feat: リダイレクト機能の追加

--- lib/app.rb
+++ lib/app.rb
@@ -1,10 +1,43 @@
 require "rack"
 
-class Application
-  def run!
-    app = Proc.new { |env| ['200', {'Content-Type' => 'text/html'}, ["<h1>Hello World</h1>"]] }
-    Rack::Handler::WEBrick.run app
+class Redirect
+  def initialize(app, from:, to:)
+    @app = app
+    @from = from
+    @to = to
+  end
+
+  def call(env)
+    if env["PATH_INFO"] == @from
+      [301, {"Location" => @to}, []]
+    else
+      @app.call(env)
+    end
   end
 end
 
-Application.new.run!
+class App
+  def call(env)
+    if env["PATH_INFO"] == "/hello"
+      status = 200
+      headers = {"Content-Type" => "text/html"}
+      body = ["<h1>Hello World!</h1>"]
+      [status, headers, body]
+    else
+      status = 404
+      headers = {"Content-Type" => "text/html"}
+      body = ["<h1>Not Found</h1>"]
+      [status, headers, body]
+    end
+  end
+end
+
+app = Rack::Builder.new {
+  run Redirect.new(
+    App.new,
+    from: "/",
+    to: "/hello"
+  )
+}
+
+Rack::Handler::WEBrick.run app
